5fb1bf2f662349601d42c92f678d626f0e59bb1f
pavankumar.m <pavankumar.m@zohocorp.com>
2023-02-21 22:35:10 +0530

    L432 - Casting in java

---
+++ src/PrimitiveTypes/Casting.java
@@ -0,0 +1,25 @@
+package PrimitiveTypes;
+
+public class Casting {
+
+    public static void main(String[] args) {
+
+        int myMinIntValue = Integer.MIN_VALUE;
+
+        int myTotal = (myMinIntValue / 2);
+        System.out.println(myTotal);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+
+        //byte myNewByteValue= (myMinByteValue / 2); Compiler throws an error here as the content is the parantheses is treated as an Int which is the default whole number in Java
+        //To overcome the above error, we use Typecasting :-
+        byte myNewByteValue= (byte)(myMinByteValue / 2);
+        System.out.println(myNewByteValue);
+
+        //Similarly for short data type
+        short myMinShortValue = Short.MIN_VALUE;
+        short myNewShortValue = (short)(myMinShortValue / 2);
+        System.out.println(myNewShortValue);
+    }
+
+}
