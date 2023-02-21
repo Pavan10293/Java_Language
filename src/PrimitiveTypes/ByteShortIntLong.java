f2890eb058cce63b98819f2cd76f4f237efa7add
pavankumar.m <pavankumar.m@zohocorp.com>
2023-02-21 22:20:27 +0530

    L431 - byte, short, long, width

--- src/PrimitiveTypes/ByteShortIntLong.java
+++ src/PrimitiveTypes/ByteShortIntLong.java
@@ -16,6 +16,32 @@ public class ByteShortIntLong {
         int myMaxIntTest2 = 2_147_483_647; //Underscores can be used to make it more readable.
         System.out.println(myMaxIntTest+" " +myMaxIntTest2);
         //int myMaxIntTest = 2147483648; Compiler flags off an error as the range of int datatype exceeds
+
+        //BYTE
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte minimum value = " + myMinByteValue);
+        System.out.println("Byte maximum value = " + myMaxByteValue);
+
+        //SHORT
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short minimum value = " + myMinShortValue);
+        System.out.println("Short maximum value = " + myMaxShortValue);
+
+        //LONG
+
+        long myLongValue = 100;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long minimum value = " + myMinLongValue);
+        System.out.println("Long maximum value = " + myMaxLongValue);
+
+        //long bigLongLiteralValue = 2_147_483_6479; Compiler throws error as the Default whole number in Java is an integer and the given no. exceeds the max value of integer.
+        long bigLongLiteralValue = 2_147_483_6479L; //"L" is required at the end to instruct the compiler that the value should be considered as a Long data type.
+        System.out.println(bigLongLiteralValue);
     }
 
 }
