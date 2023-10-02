aa9e5a877f4cadf37e2c180b4d8b446036cb74a4
pavankumar.m <pavankumar.m@zohocorp.com>
2023-10-02 10:59:53 +0530

    Balanced brackets interview problem solution.

--- src/Interview_Problems/Balanced_Brackets.java
+++ src/Interview_Problems/Balanced_Brackets.java
@@ -3,24 +3,65 @@ package Interview_Problems;
 import java.util.Stack;
 import java.lang.Character;
 
+/*given a string containing only characters '(', ')', '{', '}', '[' and ']',
+and you need to determine if the brackets in the string are balanced.
+This means that every opening bracket must have a corresponding closing bracket,
+and they must be in the correct order.
+ */
 public class Balanced_Brackets {
 
         public static void main(String[] args) {
 
-            String input = "{{[]{}}}";
-            Stack<Character> stackofBrackets = new Stack<Character>();
-            for(Character eachCharacter : input.toCharArray()) {
-                if(eachCharacter.equals('{') || eachCharacter.equals('[')) {
-                    stackofBrackets.push(eachCharacter);
+            String input = "{[()]}";
+            System.out.println(isBalancedBracketsCorrectOrder(input));
+            String input2 = "{[(])}";
+            System.out.println(isBalancedBracketsCorrectOrder(input2));
+            System.out.println(isBalancedBracketsAnyOrder(input2));
+        }
+
+        private static boolean isBalancedBracketsCorrectOrder(String inputString) {
+            Stack<Character> stack = new Stack<>();
+            for (char bracket : inputString.toCharArray()) {
+                if (bracket == '(' || bracket == '{' || bracket == '[') {
+                    stack.push(bracket);
+                } else if (bracket == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else if (bracket == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                    stack.pop();
+                } else if (bracket == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false; // mismatched bracket, not balanced
                 }
-                else {
-                    if(stackofBrackets.size()>0) {
-                        stackofBrackets.pop();
+            }
+            return stack.isEmpty(); // true if stack is empty (all brackets are balanced)
+        }
+
+        private static boolean isBalancedBracketsAnyOrder(String inputString) {
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < inputString.length(); i++) {
+                char bracket = inputString.charAt(i);
+                if (bracket == '(' || bracket == '{' || bracket == '[') {
+                    stack.push(i); // store the index of opening brackets
+                } else if (bracket == ')' || bracket == '}' || bracket == ']') {
+                    if (stack.isEmpty()) {
+                        return false; // no matching opening bracket
+                    }
+                    int top = stack.pop();
+                    char openBracket = inputString.charAt(top);
+                    if ((bracket == ')' && openBracket != '(') ||
+                            (bracket == '}' && openBracket != '{') ||
+                            (bracket == ']' && openBracket != '[')) {
+                        System.out.println("Mismatched brackets at index: " + top + " and " + i);
+                        return false; // mismatched opening and closing brackets
                     }
                 }
             }
-            System.out.println(stackofBrackets.size() == 0);
+            if (!stack.isEmpty()) {
+                System.out.println("Unclosed brackets at index: " + stack.pop());
+                return false; // unclosed opening brackets
+            }
+            return true;
         }
-
 
 }
