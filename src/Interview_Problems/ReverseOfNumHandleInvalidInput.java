832f1a9acb537ebe1262623c5f15a62a6c8ff9bc
pavankumar.m <pavankumar.m@zohocorp.com>
2023-10-02 10:17:36 +0530

    Handle invalid integer input until valid input is entered.

--- src/Interview_Problems/ReverseOfNumHandleInvalidInput.java
+++ src/Interview_Problems/ReverseOfNumHandleInvalidInput.java
@@ -1,7 +1,28 @@
 package Interview_Problems;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ReverseOfNumHandleInvalidInput {
 
-    
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            boolean validInput = false;
+
+            while (!validInput) {
+                try {
+                    System.out.print("Enter an integer: ");
+                    int number = scanner.nextInt();
+                    System.out.println("You entered: " + number);
+                    validInput = true; // Exit the loop if valid input is provided
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a valid integer.");
+                    scanner.nextLine(); // Consume the invalid input
+                }
+            }
+
+            scanner.close();
+        }
+
 
 }
