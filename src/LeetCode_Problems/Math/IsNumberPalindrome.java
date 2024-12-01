2da07665e55f7789bfe1be17def716851a03c37f
pavankumar.m <pavankumar.m@zohocorp.com>
2024-12-01 16:23:00 +0530

    LeetCode Easy level Palindrome Number problem solution

---
+++ src/LeetCode_Problems/Math/IsNumberPalindrome.java
@@ -0,0 +1,29 @@
+package LeetCode_Problems.Math;
+
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+
+public class IsNumberPalindrome {
+
+    public static boolean isPalindrome(int x) {
+        if(x>0) {
+            int original = x;
+            int reverse = 0;
+            while (x != 0) {
+                int digit = x % 10;
+                reverse = reverse * 10 + digit;
+                x /= 10;
+            }
+            return original == reverse;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(365));
+    }
+}
