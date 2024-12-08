3737bbedca19afd69020d9249c832147c794b8f5
pavankumar.m <pavankumar.m@zohocorp.com>
2024-12-08 15:07:03 +0530

    Two Sum problem solution optimised for Time complexity.

--- src/LeetCode_Problems/Arrays/TwoSum_1.java
+++ src/LeetCode_Problems/Arrays/TwoSum_1.java
@@ -1,6 +1,8 @@
 package LeetCode_Problems.Arrays;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -16,7 +18,11 @@ public class TwoSum_1 {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15}, 22)));
         System.out.println(Arrays.toString(twoSum(new int[]{3,2,4}, 6)));
+        System.out.println(Arrays.toString(twoSumHashMap(new int[]{24,7,9,10}, 16)));
     }
+
+    //Time complexity: O(n^2)
+    //Space Complexity: O(1)
 
     public static int[] twoSum(int[] nums, int target) {
         for (int i=0; i<nums.length-1; i++){
@@ -28,4 +34,20 @@ public class TwoSum_1 {
         }
         return null;
     }
+
+    //Time complexity: O(n)
+    //Space Complexity: O(n)
+
+        public static int[] twoSumHashMap(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int complement = target - nums[i];
+                if (map.containsKey(complement)) {
+                    return new int[] { map.get(complement), i };
+                }
+                map.put(nums[i], i);
+            }
+            // Return an empty array if no solution is found
+            return new int[] {};
+        }
 }
